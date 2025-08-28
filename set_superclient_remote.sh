e06f193fc524114dc8dc963752917b2f94d7a32e
Kevin Nickels <knickels@trinity.edu>
2025-08-28 14:47:55 -0500

    need profile file for introspection

--- set_superclient_remote.sh
+++ set_superclient_remote.sh
@@ -1,4 +1,4 @@
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export ROS_DISCOVERY_SERVER="131.194.115.252:11811"
-#export FASTRTPS_DEFAULT_PROFILES_FILE=/home/knickels/code/ros2_ws/src/ros2_rover/super_client_config_remote.xml
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/knickels/code/ros2_ws/src/ros2_rover/super_client_config_remote.xml
 #export ROS_SUPER_CLIENT=true
